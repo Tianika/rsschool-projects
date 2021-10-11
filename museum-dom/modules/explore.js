1159705a29b27cdffcfd34267548c007d8e04f92
Tianika <tatyriana@yandex.ru>
2021-10-11 22:35:52 +0300

    feat: add playbackrate

--- museum-dom/modules/explore.js
+++
@@ -1,9 +0,0 @@
-export function exploreSlider() {
-  let sliderInput = document.querySelector('.explore-slider input')
-  let sliderImage = document.querySelector('.original-image')
-
-  sliderInput.addEventListener('input', function () {
-    sliderImage.style.width = this.value + '%'
-    sliderInput.style.backgroundPosition = this.value + '%'
-  })
-}
