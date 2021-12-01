24b271ac5efec388ba34ccaea234dd42e6357fea
Tianika <tatyriana@yandex.ru>
2021-12-01 11:40:40 +0300

    init: start task migtarion to typescript

---
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: 'ccc40a4f31894080b74a4f2df04984da', // получите свой ключ https://newsapi.org/
+        });
+    }
+}
+
+export default AppLoader;
