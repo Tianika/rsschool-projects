f064f77f05c26dcad40421aaa35b74d0545c8cbd
Tianika <tatyriana@yandex.ru>
2021-12-05 15:15:25 +0300

    feat: add footer

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'ccc40a4f31894080b74a4f2df04984da', // получите свой ключ https://newsapi.org/
         });
     }
