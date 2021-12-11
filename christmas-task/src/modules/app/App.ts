765f54fdedab47448e867881a6d983794d15f053
Tianika <tatyriana@yandex.ru>
2021-12-11 22:00:55 +0300

    feat: add toy cards to page

--- christmas-task/src/modules/app/App.ts
+++ christmas-task/src/modules/app/App.ts
@@ -1,8 +1,14 @@
 import { Settings } from '../component/Settings'
+import { ToyCard } from '../component/ToyCard'
+import data from '../data/data'
 
 export class App {
   start(): void {
     const settingsView: Settings = new Settings()
     settingsView.draw()
+
+    const toyCards: ToyCard = new ToyCard()
+    console.log(data)
+    toyCards.draw(data)
   }
 }
--- christmas-task/src/modules/component/ToyCard.ts
+++ christmas-task/src/modules/component/ToyCard.ts
@@ -1,9 +1,47 @@
 export class ToyCard {
-  data: object
+  draw(data: []): void {
+    const fragment = document.createDocumentFragment()
+    const toyCardTemp: HTMLTemplateElement | null =
+      document.querySelector('#toyCardTemp')
 
-  constructor(data: object) {
-    this.data = data
+    data.forEach((element) => {
+      if (!toyCardTemp) return
+      const newsClone = toyCardTemp.content.cloneNode(true)
+
+      console.log(newsClone)
+
+      newsClone.querySelector('.toy-card-title').textContent = element.name
+      newsClone.querySelector(
+        '.toy-card-image'
+      ).src = `./assets/toys/${element.num}.png`
+      newsClone.querySelector(
+        '.describe-content.count'
+      ).textContent = `Количество: ${element.count}`
+      newsClone.querySelector(
+        '.describe-content.year'
+      ).textContent = `Год покупки: ${element.year}`
+      newsClone.querySelector(
+        '.describe-content.shape'
+      ).textContent = `Форма: ${element.shape}`
+      newsClone.querySelector(
+        '.describe-content.color'
+      ).textContent = `Цвет: ${element.color}`
+      newsClone.querySelector(
+        '.describe-content.size'
+      ).textContent = `Размер: ${element.size}`
+      newsClone.querySelector(
+        '.describe-content.favorite'
+      ).textContent = `Любимая: ${element.favorite ? 'Да' : 'Нет'}`
+
+      fragment.append(newsClone)
+    })
+
+    const toyContainer: HTMLElement | null =
+      document.querySelector('.toys-container')
+
+    if (!toyContainer) return
+
+    toyContainer.innerHTML = ''
+    toyContainer.appendChild(fragment)
   }
-
-  draw(data: object): void {}
 }
