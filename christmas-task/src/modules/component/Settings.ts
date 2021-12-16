0beacc54c2f0e58de7d7764f106614fbe94fce48
Tianika <tatyriana@yandex.ru>
2021-12-16 13:29:32 +0300

    refactor: change styles

--- christmas-task/src/modules/component/Settings.ts
+++ christmas-task/src/modules/component/Settings.ts
@@ -1,80 +1,74 @@
-import { Slider } from './Slider'
-import { sortAscend, sortDescend } from '../utils/sort'
-import data from '../data/data'
-import { ToyCard } from './ToyCard'
-import { sortTypes } from '../utils/constants'
+import { Slider } from './Slider';
+import { sortAscend, sortDescend } from '../utils/sort';
+import data from '../data/data';
+import { ToyCard } from './ToyCard';
+import { sortTypes } from '../utils/constants';
 
 export class Settings {
-  slider: Slider
+  slider: Slider;
 
   constructor() {
-    this.slider = new Slider()
+    this.slider = new Slider();
   }
 
   draw(): void {
-    if (this.slider) this.slider.draw()
+    if (this.slider) this.slider.draw();
 
     const favoriteChoice = document.querySelector(
       '.favorite-choice-item'
-    ) as HTMLInputElement
+    ) as HTMLInputElement;
 
     favoriteChoice.addEventListener('change', () => {
-      const toyCards = document.querySelectorAll('.toy-card')
+      const toyCards = document.querySelectorAll('.toy-card');
 
       if (favoriteChoice.checked) {
-        toyCards.forEach((card, index) => {
-          if (card.getAttribute('data-favorite') == 'false') {
-            card.classList.add('hidden')
-            setTimeout(() => {
-              card.classList.add('remove')
-            }, 330)
+        toyCards.forEach((card) => {
+          if (card.getAttribute('data-favorite') === 'false') {
+            card.classList.add('hidden');
           }
-        })
+        });
       } else {
         toyCards.forEach((card) => {
-          card.classList.remove('remove')
-          setTimeout(() => {
-            card.classList.remove('hidden')
-          }, 330)
-        })
+          card.classList.remove('hidden');
+        });
       }
-    })
+    });
 
     const selectSortType = document.querySelector(
       '.sort-choice'
-    ) as HTMLSelectElement
+    ) as HTMLSelectElement;
 
     selectSortType?.addEventListener('change', function () {
-      const index = this.selectedIndex
+      const index = this.selectedIndex;
 
       switch (index) {
         case 0:
-          sortDescend(sortTypes.default)
-          break
+          sortDescend(sortTypes.default);
+          break;
         case 1:
-          sortDescend(sortTypes.sortFromLetters)
-          break
+          sortDescend(sortTypes.sortFromLetters);
+          break;
         case 2:
-          sortAscend(sortTypes.sortFromLetters)
-          break
+          sortAscend(sortTypes.sortFromLetters);
+          break;
         case 3:
-          sortAscend(sortTypes.sortFromYear)
-          break
+          sortAscend(sortTypes.sortFromYear);
+          break;
         case 4:
-          sortDescend(sortTypes.sortFromYear)
-          break
+          sortDescend(sortTypes.sortFromYear);
+          break;
       }
-    })
+    });
 
     const defaultButton = document.querySelector(
       '.default-settings-button'
-    ) as HTMLButtonElement
+    ) as HTMLButtonElement;
 
     defaultButton.addEventListener('click', (): void => {
       //localStorage.dataForChristmasGame = JSON.stringify(data)
 
-      const toyCard = new ToyCard()
-      toyCard.draw(data)
-    })
+      const toyCard = new ToyCard();
+      toyCard.draw(data);
+    });
   }
 }
